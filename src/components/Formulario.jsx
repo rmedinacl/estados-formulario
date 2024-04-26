b2328b5af1da66610f3eae9ca9d4c6f724512330
Rodrigo Medina P <rmedina@mountain-creative.com>
2024-04-25 22:36:13 -0400

    changes formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,31 +1,77 @@
 import { useState } from "react";
 
 const Formulario = () => {
-  
   const [nombre, setNombre] = useState("");
-  const [error, setError] = useState(false)
+  const [apellido, setApellido] = useState("");
+  const [edad, setEdad] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
 
-  const validarInput = (e) => {
-    e.preventDefault()
-    if(nombre === ''){
-      setError(true)
-      return
+  const validarDatos = (e) => {
+    e.preventDefault();
+
+    if (nombre === "" || apellido === "" || edad === "" || email === "") {
+      setError(true);
+      return;
     }
-    setError(false)
-  }
+    setError(false);
+    setNombre("");
+    setApellido("");
+    setEdad("");
+    setEmail("");
+  };
 
   return (
     <>
-      <form onSubmit={validarInput}>
-        {error ? <p className="error">Debes ingresar tu nombre</p> : null}
-        <h3>{nombre}</h3>
+      <form className="formulario" onSubmit={validarDatos}>
+        {error ? <p>Todos los campos son obligatorios</p> : null}
         <div className="form-group">
-          <input className="form-control" name="Nombre" onChange={(e) => setNombre(e.target.value)}/>
-          <button className="btn btn-dark mt-3" type="submit">
-            Enviar
-          </button>
+          <label>Nombre</label>
+          <input
+            type="text"
+            name="nombre"
+            className="form-control"
+            onChange={(e) => setNombre(e.target.value)}
+            value={nombre}
+          />
         </div>
+        <div className="form-group">
+          <label>Apellido</label>
+          <input
+            type="text"
+            name="apellido"
+            className="form-control"
+            onChange={(e) => setApellido(e.target.value)}
+            value={apellido}
+          />
+        </div>
+        <div className="form-group">
+          <label>Edad</label>
+          <input
+            type="text"
+            name="edad"
+            className="form-control"
+            onChange={(e) => setEdad(e.target.value)}
+            value={edad}
+          />
+          {}
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            className="form-control"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Enviar
+        </button>
       </form>
+      <h1>Datos ingresados</h1>
+      {nombre} - {apellido} - {edad} - {email}
     </>
   );
 };
